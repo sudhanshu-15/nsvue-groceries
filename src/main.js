91c95fe6b95d008e203088437c6466630a84b049
Sudhanshu Siddh <ssiddh@iu.edu>
2018-07-25 22:58:18 -0400

    Starting code base

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import Vue from 'nativescript-vue';
 
-import Counter from './components/Counter';
-import HelloWorld from './components/HelloWorld';
-import Login from './components/Login';
+import Login from './components/Login/Login';
 
 import store from './store';
 
 import './styles.scss';
 
 // Uncomment the following to see NativeScript-Vue output logs
-//Vue.config.silent = false;
+Vue.config.silent = false;
 
 new Vue({
 
