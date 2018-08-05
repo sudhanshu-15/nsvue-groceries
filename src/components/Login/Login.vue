ee2b0cfd8ddc9f855f68e13ad17c7158e498702d
Sudhanshu Siddh <ssiddh@iu.edu>
2018-08-05 16:43:18 -0400

    Created one way binding

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -6,7 +6,7 @@
     <FlexboxLayout>
       <StackLayout class="form">
         <image src="res://logo_login" />
-        <TextField hint="Email Address" keyboardType="email" autocorrect="false" class="input input-border"></TextField>
+        <TextField hint="Email Address" keyboardType="email" autocorrect="false" class="input input-border" :text="email"></TextField>
         <TextField hint="Password" secure="true" class="input input-border"></TextField>
         <Button text="Sign in" class="btn btn-primary" @tap="submit"></Button>
         <Button text="Sign up for Groceries"></Button>
@@ -18,9 +18,14 @@
 
 <script>
   export default {
+    data () {
+      return {
+        email: "nativescriptrocks@progress.com",
+      }
+    },
     methods: {
       submit() {
-        console.log("Hello");
+        alert("You are using:" + this.email);
       }
     }
   }
